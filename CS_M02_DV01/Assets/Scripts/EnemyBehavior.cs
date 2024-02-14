446603adb6d2a329bd5e90f16e71ca7e885f6b9d
schwermc <schwermc@my.erau.edu>
2024-02-14 13:48:34 -0700

    The Start of M02 DV01

---
+++ CS_M02_DV01/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Player detected - attack!");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Player out of ranger, resume patrol!");
+        }
+    }
+}
