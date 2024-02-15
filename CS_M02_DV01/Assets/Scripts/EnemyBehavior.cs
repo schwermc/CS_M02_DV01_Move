355696772ed0b00b67c9f49f10723aaa06d0f288
schwermc <schwermc@my.erau.edu>
2024-02-15 09:12:21 -0700

    Enemy Pathing V1

--- CS_M02_DV01/Assets/Scripts/EnemyBehavior.cs
+++ CS_M02_DV01/Assets/Scripts/EnemyBehavior.cs
@@ -1,9 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public Transform patrolRoute;
+    public List<Transform> locations;
+
+    private int locationIndex = 0;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
+
+    private void Update()
+    {
+        if (agent.remainingDistance < 0.2f && !agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach (Transform child in patrolRoute)
+        {
+            locations.Add(child);
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        if (locations.Count == 0)
+        {
+            return;
+        }
+        agent.destination = locations[locationIndex].position;
+        locationIndex = (locationIndex + 1) % locations.Count;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
